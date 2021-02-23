db288f87e8aca81a4c2dc85e6de018f6d0ce8536
Mr-JZ <jan.zisenis@gmail.com>
2021-02-23 14:45:36 +0000

    update #1

---
+++ build/CMakeFiles/PlayCat.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/PlayCat/src/CatFinder.cpp" "/home/pi/PlayCat/build/CMakeFiles/PlayCat.dir/src/CatFinder.cpp.o"
+  "/home/pi/PlayCat/src/LaserPointerFinder.cpp" "/home/pi/PlayCat/build/CMakeFiles/PlayCat.dir/src/LaserPointerFinder.cpp.o"
+  "/home/pi/PlayCat/src/MJPEGServer/MJPEGServer.cpp" "/home/pi/PlayCat/build/CMakeFiles/PlayCat.dir/src/MJPEGServer/MJPEGServer.cpp.o"
+  "/home/pi/PlayCat/src/main.cpp" "/home/pi/PlayCat/build/CMakeFiles/PlayCat.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ERROR_CODE_HEADER_ONLY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/opencv-4.1.0/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PlayCat.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PlayCat.dir/src/main.cpp.o"
+  "CMakeFiles/PlayCat.dir/src/CatFinder.cpp.o"
+  "CMakeFiles/PlayCat.dir/src/MJPEGServer/MJPEGServer.cpp.o"
+  "CMakeFiles/PlayCat.dir/src/LaserPointerFinder.cpp.o"
+  "PlayCat.pdb"
+  "PlayCat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PlayCat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
